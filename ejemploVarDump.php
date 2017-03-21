5e55a88ea5d0f8b5b867725e9e86540439789183
jmateu.daw@gmail.com <jmateu.daw@gmail.com>
2017-03-21 17:47:52 +0100

    Ejercicio vardump

---
+++ ejemploVarDump.php
@@ -0,0 +1,15 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+   
+
+   $a=6;
+   $b=7.83;
+   $c=false;
+   $d="Juan";
+
+
+   echo var_dump($a);
+   echo var_dump($b);
+   echo var_dump($c);
+   echo var_dump($d);
+?>
